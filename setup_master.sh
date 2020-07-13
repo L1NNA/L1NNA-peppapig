5b1da89ac4b6b0d14a11c4179a3831228400f6e2
Steven Ding <steven-hh-ding@users.noreply.github.com>
2020-07-13 15:31:09 -0400

    Update and rename setup_master to setup_master.sh

--- setup_master.sh
+++ setup_master.sh
@@ -7,7 +7,7 @@ EOF
 apt-get update'
 
 sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
-apt-get update
+sudo apt-get update
 
 # disable swap on the master node (sorry!, kube requires, supposing kub is the only thing we run on the server)
 sudo swapoff -a  
@@ -18,7 +18,7 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
 
 sudo apt-get install -y --allow-unauthenticated kubelet kubeadm kubectl kubernetes-cni
 sudo apt-mark hold kubelet kubeadm kubectl
@@ -29,7 +29,8 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker && sudo systemctl start docker
 sudo systemctl enable kubelet && sudo systemctl start kubelet
 
-sudo kubeadm init --apiserver-advertise-address=$1
+# sudo kubeadm init --apiserver-advertise-address=$1
+sudo kubeadm init
 
 sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
@@ -39,5 +40,9 @@ export KUBECONFIG=$HOME/admin.conf
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 sudo systemctl restart kubelet
 
-curl https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/examples/post-install.sh | bash
+# curl https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/examples/post-install.sh | bash
+
+
+# install helm:
+
 
