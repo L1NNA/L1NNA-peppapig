89266a6fa57604579e3b7a96eac23073af2a66e3
Steven Ding <steven-hh-ding@users.noreply.github.com>
2020-07-13 12:10:09 -0400

    Create setup_driver.sh

---
+++ setup_driver.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-get update
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update
+
+
+# install GPU driver:
+sudo apt-get install -y --no-install-recommends nvidia-driver-450
+
+nvidia-smi
+
+echo 'please reboot your system'
+
