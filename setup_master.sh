eea398af432bc374e258a2c9b490b24e9dbc9dc1
Steven Ding <steven-hh-ding@users.noreply.github.com>
2020-07-14 00:05:17 -0400

    Update setup_master.sh

--- setup_master.sh
+++ setup_master.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # install driver first, if this is a gpu node: 
-# curl https://raw.githubusercontent.com/L1NNA/L1NNA-cluster/master/setup_driver.sh | bash
+# curl https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/setup_driver.sh | bash
 # then:
-# curl https://raw.githubusercontent.com/L1NNA/L1NNA-cluster/master/setup_master.sh | bash
+# curl https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/setup_master.sh | bash
 
 sudo bash -c 'apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
