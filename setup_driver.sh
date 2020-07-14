e0e367fdfef56b9d9e33e2628a06a76b2a0ef72c
Steven Ding <steven-hh-ding@users.noreply.github.com>
2020-07-14 00:05:41 -0400

    Update setup_driver.sh

--- setup_driver.sh
+++ setup_driver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# curl https://raw.githubusercontent.com/L1NNA/L1NNA-cluster/master/setup_driver.sh | bash
+# curl https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/setup_driver.sh | bash
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
