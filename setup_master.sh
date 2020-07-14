6d109ed83e04b6bfdbb1b3f7fa5630c00400d1d2
Steven Ding <steven-hh-ding@users.noreply.github.com>
2020-07-13 21:19:50 -0400

    Update setup_master.sh

--- setup_master.sh
+++ setup_master.sh
@@ -3,6 +3,7 @@
 # curl https://raw.githubusercontent.com/L1NNA/L1NNA-cluster/master/setup_driver.sh | bash
 # then:
 # curl https://raw.githubusercontent.com/L1NNA/L1NNA-cluster/master/setup_master.sh | bash
+
 sudo bash -c 'apt-get update && apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
