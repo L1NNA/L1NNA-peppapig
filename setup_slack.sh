6bfd5365c46cb01dc916a80b25318abdc95c6014
Steven Ding <steven-hh-ding@users.noreply.github.com>
2020-07-18 21:18:54 -0400

    Create setup_slack.sh

---
+++ setup_slack.sh
@@ -0,0 +1,34 @@
+  
+#!/bin/sh
+# install driver first, if this is a gpu node: 
+# curl https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/setup_slack.sh | bash
+
+export KUBECONFIG=$HOME/admin.conf
+
+helm repo add infracloudio https://infracloudio.github.io/charts
+helm repo update
+
+echo "you need to obtain your slack api token by installing this app to your workspace:"
+echo ""
+read -p "Enter slack api token: " SLACK_API_TOKEN_FOR_THE_BOT
+read -p "Enter slack channel name: " SLACK_CHANNEL_NAME
+
+ALLOW_KUBECTL="yes"
+read -e -i "$ALLOW_KUBECTL" -p "Please enter your name: " input
+ALLOW_KUBECTL="${input:-$ALLOW_KUBECTL}"
+
+
+CLUSTER_NAME=peppa-pig
+
+helm install --version v0.10.0 botkube --namespace botkube \
+  --set communications.slack.enabled=true \
+  --set communications.slack.channel=$SLACK_CHANNEL_NAME \
+  --set communications.slack.token=$SLACK_API_TOKEN_FOR_THE_BOT \
+  --set config.settings.clustername=$CLUSTER_NAME \
+  --set config.settings.allowkubectl=$ALLOW_KUBECTL \
+  --set image.repository=infracloudio/botkube \
+  --set image.tag=v0.10.0 \
+  infracloudio/botkube
+  
+kubectl get pods -n infracloudio
+  
