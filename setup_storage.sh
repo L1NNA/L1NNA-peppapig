0a9d97a0bef7d7e54e0b57b51a8f143cc2d5a6cd
Steven Ding <steven-hh-ding@users.noreply.github.com>
2020-09-03 16:24:21 -0400

    Update setup_storage.sh

--- setup_storage.sh
+++ setup_storage.sh
@@ -5,17 +5,49 @@ export KUBECONFIG=$HOME/admin.conf
 
 
 echo 'setting up storage class'
-kubectl create -f https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/local-hdd.yaml
-
+kubectl create -f https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/local-hdd-lg.yaml
+kubectl create -f https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/local-hdd-md.yaml
+kubectl create -f https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/local-hdd-sm.yaml
 
 echo 'setting up PV according to the volums at /media/'
-for D in /media/*; do
+for D in /media/sm/*; do
     if [ -d "${D}" ]; then
         par="${D}"
         name='v'${par//"/"/"-"}
         size=$(sudo df -BG --output=avail $par| grep -v Avail | xargs)
         echo $par "=>" $name $size "bytes"   
-        patch1='{"metadata":{"name":"'$name'"},"spec":{"capacity":{"storage":"'$size'i"},"local":{"path":"'$par'"},"nodeAffinity":{"required":{"nodeSelectorTerms":[{"matchExpressions":[{"key":"kubernetes.io/hostname","operator":"In","values":["'$(hostname)'"]}]}]}}}}}'
+        patch1='{"metadata":{"name":"'$name'"},"spec":{"capacity":{"storage":"'$size'i"}, "storageClassName":"local-hdd-sm", "local":{"path":"'$par'"},"nodeAffinity":{"required":{"nodeSelectorTerms":[{"matchExpressions":[{"key":"kubernetes.io/hostname","operator":"In","values":["'$(hostname)'"]}]}]}}}}}'
+        kubectl patch -f https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/local-hdd-pv.yaml -p $patch1 --dry-run=client -o yaml | kubectl create -f -
+        # kubectl create -f local-hdd-pv.yaml
+        echo $patch1
+        # break
+    fi
+done
+
+echo 'setting up PV according to the volums at /media/'
+for D in /media/md/*; do
+    if [ -d "${D}" ]; then
+        par="${D}"
+        name='v'${par//"/"/"-"}
+        size=$(sudo df -BG --output=avail $par| grep -v Avail | xargs)
+        echo $par "=>" $name $size "bytes"   
+        patch1='{"metadata":{"name":"'$name'"},"spec":{"capacity":{"storage":"'$size'i"}, "storageClassName":"local-hdd-md", "local":{"path":"'$par'"},"nodeAffinity":{"required":{"nodeSelectorTerms":[{"matchExpressions":[{"key":"kubernetes.io/hostname","operator":"In","values":["'$(hostname)'"]}]}]}}}}}'
+        kubectl patch -f https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/local-hdd-pv.yaml -p $patch1 --dry-run=client -o yaml | kubectl create -f -
+        # kubectl create -f local-hdd-pv.yaml
+        echo $patch1
+        # break
+    fi
+done
+
+
+echo 'setting up PV according to the volums at /media/'
+for D in /media/lg/*; do
+    if [ -d "${D}" ]; then
+        par="${D}"
+        name='v'${par//"/"/"-"}
+        size=$(sudo df -BG --output=avail $par| grep -v Avail | xargs)
+        echo $par "=>" $name $size "bytes"   
+        patch1='{"metadata":{"name":"'$name'"},"spec":{"capacity":{"storage":"'$size'i"}, "storageClassName":"local-hdd-lg", "local":{"path":"'$par'"},"nodeAffinity":{"required":{"nodeSelectorTerms":[{"matchExpressions":[{"key":"kubernetes.io/hostname","operator":"In","values":["'$(hostname)'"]}]}]}}}}}'
         kubectl patch -f https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/local-hdd-pv.yaml -p $patch1 --dry-run=client -o yaml | kubectl create -f -
         # kubectl create -f local-hdd-pv.yaml
         echo $patch1
