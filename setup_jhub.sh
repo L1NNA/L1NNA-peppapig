84910166b0752786cb24d6d3eeb0200908d33de4
Steven Ding <steven-hh-ding@users.noreply.github.com>
2020-09-04 00:43:15 -0400

    Update setup_jhub.sh

--- setup_jhub.sh
+++ setup_jhub.sh
@@ -6,10 +6,10 @@ export KUBECONFIG=$HOME/admin.conf
 
 # create storage class
 wget https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/config.yaml
-sed -i -e 's/peppa-tkn/'$(openssl rand -hex 32)'/g' /config.yaml
+sed -i -e 's/peppa-tkn/'$(openssl rand -hex 32)'/g' ./config.yaml
 
 read -p "Enter slack notification web nook: " SLACK_WEBHOOK
-sed -i -e 's,peppa-webhook,'$SLACK_WEBHOOK',g' /config.yaml
+sed -i -e 's,peppa-webhook,'$SLACK_WEBHOOK',g' ./config.yaml
 
 
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
