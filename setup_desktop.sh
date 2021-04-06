1b0c4692620ba0647a7bcc1f2d6d2b17a27f40f8
Steven Ding <steven-hh-ding@users.noreply.github.com>
2021-04-06 12:09:50 -0400

    Update setup_desktop.sh

--- setup_desktop.sh
+++ setup_desktop.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
-# curl https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/setup_driver.sh | bash
+# curl https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/setup_desktop.sh | bash
 
-DIR="/home/jovyan/.config/xfce4"
 
-if [ -d "$DIR" ]; then
+wget -O /home/jovyan/.condarc https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/.condarc
+sudo wget -O /opt/conda/.condarc https://raw.githubusercontent.com/L1NNA/L1NNA-peppapig/master/.condarc-opt
+
+DIR="/home/jovyan/.config/"
+
+if [ -d "$DIR/xfce4" ]; then
   # Take action if $DIR exists. #
   echo "Initizlized.. skipping..."
 else
   ###  Control will jump here if $DIR does NOT exists ###
   echo "Initizlizing..."
   mkdir -p "$DIR"
-  wget -O $DIR/xfconf.zip https://github.com/L1NNA/L1NNA-peppapig/releases/download/dot/xfconf.zip
+  wget -O $DIR/xfce4.zip https://github.com/L1NNA/L1NNA-peppapig/releases/download/dot/xfce4.zip
   unzip $DIR/xfconf.zip -d $DIR/
 fi
